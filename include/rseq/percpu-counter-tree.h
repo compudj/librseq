d1050be9f9169e3c47281a683e6db1006f7f3079
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2025-04-02 21:19:56 -0400

    Implement Hierarchical Per-CPU Counters
    This hierarchical per-cpu counters implementation is based on
    single-byte counters. It minimizes the memory and cache footprint needed
    to track the precise and approximate sum.
    
    * Motivation
    
    The purpose of this hierarchical split-counter scheme is to:
    
    - Minimize contention when incrementing and decrementing counters,
    - Provide fast access to a sum approximation,
    - Provide a sum approximation with an acceptable accuracy level when
      scaling to many-core systems.
    - Provide approximate and precise comparison of two counters, and
      between a counter and a value.
    
    * Design
    
    The hierarchical per-CPU counters propagate a sum approximation through
    a N-way tree. When reaching the batch size, the carry is propagated
    through a binary tree which consists of logN(nr_cpu_ids) levels. The
    batch size for each level is twice the batch size of the prior level.
    
    Example propagation diagram with 8 cpus through a binary tree:
    
    Level 0:  0    1    2    3    4    5    6    7
              |   /     |   /     |   /     |   /
              |  /      |  /      |  /      |  /
              | /       | /       | /       | /
    Level 1:  0         1         2         3
              |       /           |       /
              |    /              |    /
              | /                 | /
    Level 2:  0                   1
              |               /
              |         /
              |   /
    Level 3:  0
    
    For a binary tree, the maximum inaccuracy is bound by:
       batch_size * log2(nr_cpus) * nr_cpus
    which evolves with O(n*log(n)) as the number of CPUs increases.
    
    For a N-way tree, the maximum inaccuracy can be pre-calculated
    based on the the N-arity of each level and the batch size
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Change-Id: Ic6e87bff5a062e5f74f23fefba1ca465cbe18cbc

---
+++ include/rseq/percpu-counter-tree.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: MIT */
+/* SPDX-FileCopyrightText: 2025 Mathieu Desnoyers <mathieu.desnoyers@efficios.com> */
+
+#ifndef _RSEQ_PERCPU_COUNTER_TREE_H
+#define _RSEQ_PERCPU_COUNTER_TREE_H
+
+# ifdef __cplusplus
+extern "C" {
+# endif
+
+struct percpu_counter_tree;
+
+/* Fast paths */
+void percpu_counter_tree_add(struct percpu_counter_tree *counter, long inc);
+long percpu_counter_tree_approximate_sum(struct percpu_counter_tree *counter);
+
+/* Slow paths */
+struct percpu_counter_tree *percpu_counter_tree_alloc(unsigned long batch_size);
+void percpu_counter_tree_destroy(struct percpu_counter_tree *counter);
+long percpu_counter_tree_precise_sum(struct percpu_counter_tree *counter);
+int percpu_counter_tree_approximate_compare(struct percpu_counter_tree *a, struct percpu_counter_tree *b);
+int percpu_counter_tree_approximate_compare_value(struct percpu_counter_tree *counter, long v);
+int percpu_counter_tree_precise_compare(struct percpu_counter_tree *a, struct percpu_counter_tree *b);
+int percpu_counter_tree_precise_compare_value(struct percpu_counter_tree *counter, long v);
+void percpu_counter_tree_set_bias(struct percpu_counter_tree *counter, long bias);
+void percpu_counter_tree_set(struct percpu_counter_tree *counter, long v);
+unsigned long percpu_counter_tree_inaccuracy(struct percpu_counter_tree *counter);
+unsigned int percpu_counter_tree_get_depth(struct percpu_counter_tree *counter);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* _RSEQ_PERCPU_COUNTER_TREE_H */
